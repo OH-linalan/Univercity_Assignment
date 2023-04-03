cd7c864eeb96c908232b8339120b01b045014911
OH-linalan <linalan@kw.ac.kr>
2023-04-03 06:34:48 +0000

    basic structure

---
+++ Data-Structure/Linked_list.cpp
@@ -0,0 +1,31 @@
+/*
+  summary: linked lists
+*/
+//header
+#include <iostream>
+//using
+using namespace std;
+//class
+class Node {
+    public:
+        int data;
+        Node* next;
+};
+class list {
+    private:
+        Node* head;
+    public:
+        list(void){
+            head = NULL;
+        }~list(void);
+        void push(int element)
+};
+//global variables
+
+//functions
+
+//main
+int main(){
+    
+    return 0;
+}
